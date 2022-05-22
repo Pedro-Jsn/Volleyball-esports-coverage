f6daad3c0b89dd7d0718440dfa1d97a5c0739268
Pedro-Jsn <pedro.varela@sptech.school>
2022-05-22 01:01:49 -0300

    Finalização das telas

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,12 @@
 function noturno(){
-    var color = document.getElementById("night_text").innerText;
+    var texto = document.getElementById("night_text").innerText;
 
-    if (color == "OFF") {
+    if (texto == "OFF") {
         document.body.style.setProperty('--main-color', '#2f3337');
         document.body.style.setProperty('--background-color', '#2f3337');
         document.body.style.setProperty('--text-color', '#c6c6c6');
+        document.body.style.setProperty('--card-color', '#40474f');
+        document.body.style.setProperty('--destaque-color', '#F2BB16');
 
         night_text.style.color = 'green';
         document.getElementById("night_text").innerHTML = "ON";
@@ -12,6 +14,9 @@ function noturno(){
         document.body.style.setProperty('--main-color', '#dadadc');
         document.body.style.setProperty('--background-color', '#c6c6c6');
         document.body.style.setProperty('--text-color', '#363636');
+        document.body.style.setProperty('--card-color', '#fafafa');
+        document.body.style.setProperty('--destaque-color', '#030A8C');
+        
 
         night_text.style.color = 'red';
         document.getElementById("night_text").innerHTML = "OFF";
